fa3870ef00880c2056a9ef8eab081b87ba2dc629
Daniel Gustafson <danielgustafson@Daniels-MacBook-Pro.local>
2019-09-20 05:50:25 -0700

    help button

--- force-app/main/default/aura/enhancedSharingRules/enhancedSharingRulesController.js
+++ force-app/main/default/aura/enhancedSharingRules/enhancedSharingRulesController.js
@@ -1,5 +1,14 @@
 ({
     myAction : function(component, event, helper) {
 
-    }
+    },
+    openModel: function(component, event, helper) {
+        // Set isModalOpen attribute to true
+        component.set("v.isModalOpen", true);
+     },
+    
+     closeModel: function(component, event, helper) {
+        // Set isModalOpen attribute to false  
+        component.set("v.isModalOpen", false);
+     }
 })
